3ebd6fe1c8ad0cad5d51dc80fbd137c12a88b013
Adam N <adam.nawroc@gmail.com>
2019-01-03 16:26:46 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -4,10 +4,14 @@ import React from 'react';
 export default class DelayedButton extends React.Component {
 
   tickle = (event) => {
-   let arr = [event.clientX, event.clientY]
-   this.props.onReceiveCoordinates(arr)
+  //  let arr = [event.clientX, event.clientY]
+
+  setTimeout(function(){ this.props.onDelayedClick(event) }, this.props.delay);
+
+
+   
  }
-  
+
   render() {
       return (
         <button onClick={this.tickle}>Tickle me!</button>
