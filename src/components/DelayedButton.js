cdce3b6116a23738da99fcf7e02f471153cee127
Adam N <adam.nawroc@gmail.com>
2019-01-03 16:23:27 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -2,13 +2,10 @@
 import React from 'react';
 
 export default class DelayedButton extends React.Component {
-<<<<<<< HEAD
   render() {
       return (
         <button>Tickle me!</button>
       );
     }
-=======
 
->>>>>>> f585fad6f58b58b5ddd9422279223745dce70188
 }
