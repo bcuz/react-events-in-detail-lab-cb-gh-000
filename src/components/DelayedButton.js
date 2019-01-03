b8ed0536277285219ae40cb141251b08e7778056
Adam N <adam.nawroc@gmail.com>
2019-01-03 16:39:16 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -4,6 +4,8 @@ export default class DelayedButton extends React.Component {
 
   tickle = (event) => {
   //  let arr = [event.clientX, event.clientY]
+
+  // need this to access event in timeout
   event.persist()
 // feel like this should work.
   setTimeout(() => { this.props.onDelayedClick(event) }, this.props.delay);
