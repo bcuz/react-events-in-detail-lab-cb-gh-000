6af345e51d91bb1c59b3053ce5b15ebd11c8c0f8
Adam N <adam.nawroc@gmail.com>
2019-01-03 16:22:32 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 export default class CoordinatesButton extends React.Component {
 
+<<<<<<< HEAD
   tickle = (event) => {
    let arr = [event.clientX, event.clientY]
    this.props.onReceiveCoordinates(arr)
@@ -15,4 +16,6 @@ export default class CoordinatesButton extends React.Component {
         <button onClick={this.tickle}>Tickle me!</button>
       );
     }
+=======
+>>>>>>> f585fad6f58b58b5ddd9422279223745dce70188
 }
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -2,9 +2,13 @@
 import React from 'react';
 
 export default class DelayedButton extends React.Component {
+<<<<<<< HEAD
   render() {
       return (
         <button>Tickle me!</button>
       );
     }
+=======
+
+>>>>>>> f585fad6f58b58b5ddd9422279223745dce70188
 }
